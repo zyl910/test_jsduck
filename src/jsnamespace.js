e3bcc6e964291dbe4e0e7547dcf96e3750670632
zyl910 <zyl910@sina.com>
2017-11-07 15:51:30 +0800

    create jsnamespace.js

---
+++ src/jsnamespace.js
@@ -0,0 +1,8 @@
+/*! @file jsnamespace.js
+ * 演示JavaScript中如何模拟命名空间，并支持 构造函数法、闭包法 来构造类. 还演示了 jsduck 文档注释.
+ *
+ * @author zhouyuelin
+ * @version v1.0
+ */
+
+var jsnamespace = window.jsnamespace || {};
