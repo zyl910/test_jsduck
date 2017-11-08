08916630f4ca76fcec11cc7f0eafb3df86645596
zyl910 <zyl910@sina.com>
2017-11-08 19:28:43 +0800

    测试子命名空间与字命名空间里的极简模式的类.

--- src/jsnamespace.js
+++ src/jsnamespace.js
@@ -17,8 +17,11 @@ var jsnamespace = window.jsnamespace || {};
  * 性别代码. 枚举类.
  */
 jsnamespace.GenderCode = {
+	/** 未知 */
 	"UNKNOWN": 0,
+	/** 男 */
 	"MALE": 1,
+	/** 女 */
 	"FEMALE": 2
 };
 
@@ -98,3 +101,58 @@ jsnamespace.PersonInfoUtil = function () {
 	};
 }();
 
+
+// == jsnamespace.sub ==
+
+/** @class
+ * 子命名空间.
+ * @abstract
+ */
+jsnamespace.sub = window.jsnamespace.sub || {};
+
+// 极简主义法（minimalist approach）定义类.
+
+/**
+ * 动物.
+ */
+jsnamespace.sub.Animal = {
+	/** 创建 动物.
+	 *
+	 * @return  {Animal}	返回所创建的对象.
+	 * @static
+	 */
+	createNew: function(){
+		var animal = {};
+		/** 睡觉.
+		 */
+		animal.sleep = function(){ alert("睡懒觉"); };
+		return animal;
+	}
+};
+
+/**
+ * 猫.
+ * @extends jsnamespace.sub.Animal
+ */
+jsnamespace.sub.Cat = {
+	/** 声音.
+	 * @static @protected
+	 */
+	sound : "喵喵喵",
+	/** 创建 猫.
+	 *
+	 * @return  {Cat}	返回所创建的对象.
+	 * @static
+	 */
+	createNew: function(){
+		var cat = jsnamespace.sub.Animal.createNew();
+		/** 发声.
+		 */
+		cat.makeSound = function(){ alert(jsnamespace.sub.Cat.sound); };
+		/** 修改声音.
+		 * @param {String}	x	声音.
+		 */
+		cat.changeSound = function(x){ jsnamespace.sub.Cat.sound = x; };
+		return cat;
+	}
+};
