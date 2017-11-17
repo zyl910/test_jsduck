a670bee1e5d52de9809d3525c17add925ff52f42
zyl910 <zyl910@sina.com>
2017-11-17 15:47:17 +0800

    模拟封装与继承.

--- src/jsnamespace.js
+++ src/jsnamespace.js
@@ -11,6 +11,20 @@
  */
 var jsnamespace = window.jsnamespace || {};
 
+/** 继承. 即设置好 Child 的原型为 Parent的原型实例，并设置 uber 属性.
+ * 
+ *  @param	{Function}	Child	子类（构造函数）.
+ *  @param	{Function}	Parent	父类（构造函数）.
+ *	@static
+ */
+jsnamespace.extend = function(Child, Parent) {
+	var F = function(){};
+	F.prototype = Parent.prototype;
+	Child.prototype = new F();
+	Child.prototype.constructor = Child;
+	Child.uber = Parent.prototype;
+};
+
 // == enum ==
 
 /** @enum
@@ -33,38 +47,74 @@ jsnamespace.GenderCode = {
  */
 jsnamespace.PersonInfo = function(cfg) {
 	cfg = cfg || {};
-	/** @cfg {String} [name=""] 姓名. */
 	/** @property {String} 姓名. */
 	this.name = cfg["name"] || "";
-	/** @cfg {jsnamespace.GenderCode} [gender=jsnamespace.GenderCode.UNKNOWN] 性别. */
 	/** @property {jsnamespace.GenderCode} 性别. */
 	this.gender = cfg["gender"] || jsnamespace.GenderCode.UNKNOWN;
 };
+(function(){
 
-/**
- * 取得称谓.
- *
- * @return  {String}	返回称谓字符串.
- */
-jsnamespace.PersonInfo.prototype.getAppellation = function() {
-	var rt = "";
-	if (jsnamespace.GenderCode.MALE == this.gender) {
-		rt = "Mr.";
-	} else if (jsnamespace.GenderCode.FEMALE == this.gender) {
-		rt = "Ms.";
-	}
-	return rt;
-};
+	/**
+	 * 取得称谓.
+	 *
+	 * @return  {String}	返回称谓字符串.
+	 */
+	jsnamespace.PersonInfo.prototype.getAppellation = function() {
+		var rt = "";
+		if (jsnamespace.GenderCode.MALE == this.gender) {
+			rt = "Mr.";
+		} else if (jsnamespace.GenderCode.FEMALE == this.gender) {
+			rt = "Ms.";
+		}
+		return rt;
+	};
 
-/**
- * 取得欢迎字符串.
- *
- * @return  {String}	返回欢迎字符串.
+	/**
+	 * 取得欢迎字符串.
+	 *
+	 * @return  {String}	返回欢迎字符串.
+	 */
+	jsnamespace.PersonInfo.prototype.getHello = function() {
+		var rt = "Hello, " + this.getAppellation() + " " + this.name;
+		return rt;
+	};
+
+	// -- static method --
+	/** 显示信息.
+	 *
+	 * @param {jsnamespace.PersonInfo}	p	个人信息.
+	 * @static
+	 */
+	jsnamespace.PersonInfo.show = function(p) {
+		var s = p.getHello();
+		alert(s);
+	};
+	
+})();
+
+// == Employee class ==
+
+/** @class
+ * 雇员信息. 构造函数法的类.
  */
-jsnamespace.PersonInfo.prototype.getHello = function() {
-	var rt = "Hello, " + this.getAppellation() + " " + (this.name);
-	return rt;
+jsnamespace.Employee = function(cfg) {
+	cfg = cfg || {};
+	jsnamespace.PersonInfo.call(this, cfg);
+	// 自身的实例字段.
+	/** @property {String} 电子邮箱. */
+	this.email = cfg["email"] || "";
 };
+jsnamespace.extend(jsnamespace.Employee, jsnamespace.PersonInfo);
+(function(){
+
+	/** @inheritdoc */
+	jsnamespace.Employee.prototype.getHello = function() {
+		var rt = jsnamespace.PersonInfo.prototype.getHello.call(this);
+		rt = rt + " (" + this.email + ")";
+		return rt;
+	};
+	
+})();
 
 
 // == PersonInfoUtil class ==
